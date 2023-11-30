5a7381b0ce414929b5d256df4587a5b1abdc038d
Juan Manuel N.C <jmnavarrete@robotnik.es>
2023-11-30 18:21:38 +0100

    Modification to detect Internet connection failures and retry to connect. Added a topic to indicate when you are getting NTRIP fixes.

--- src/ntrip_client/ntrip_client.py
+++ src/ntrip_client/ntrip_client.py
@@ -90,6 +90,10 @@ class NTRIPClient:
     self.reconnect_attempt_wait_seconds = self.DEFAULT_RECONNECT_ATEMPT_WAIT_SECONDS
     self.rtcm_timeout_seconds = self.DEFAULT_RTCM_TIMEOUT_SECONDS
 
+    #Control flags
+    self.in_timeout_flag = False
+    self.in_blocking_io_error_flag = False
+
   def connect(self):
     # Create a socket object that we will use to connect to the server
     self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -247,8 +251,12 @@ class NTRIPClient:
       self._first_rtcm_received = False
 
     # Check if there is any data available on the socket
-    read_sockets, _, _ = select.select([self._server_socket], [], [], 0)
-    if not read_sockets:
+    try:
+      read_sockets, _, _ = select.select([self._server_socket], [], [], 0)
+      if not read_sockets:
+        return []
+    except Exception as e:
+      self._logerr('Error while checking for data on socket')
       return []
 
     # Since we only ever pass the server socket to the list of read sockets, we can just read from that
@@ -310,15 +318,24 @@ class NTRIPClient:
       data = self._server_socket.recv(_CHUNK_SIZE, socket.MSG_DONTWAIT | socket.MSG_PEEK)
       if len(data) == 0:
         return False
-    except BlockingIOError:
+    except BlockingIOError as exBlockingIOError:
+      self.in_blocking_io_error_flag = True
+      self._logwarn('BlockingIOError. Ex:{}'.format(exBlockingIOError))
       return True  # socket is open and reading from it would block
-    except ConnectionResetError:
-      self._logwarn('Connection reset by peer')
+    except ConnectionResetError as exConnectionResetError:
+      self._logwarn('Connection reset by peer. Ex:{}'.format(exConnectionResetError))
       return False  # socket was closed for some other reason
-    except socket.timeout:
+    except socket.timeout as exTimeOut:
+      self.in_timeout_flag = True
+      self._logwarn('Socket timeout. Ex: {}'.format(exTimeOut))
       return True  # timeout likely means that the socket is still open
     except Exception as e:
       self._logwarn('Socket appears to be closed')
       self._logwarn('Exception: {}'.format(e))
       return False
+    self.in_blocking_io_error_flag = False
+    self.in_timeout_flag = False
     return True
+
+  def is_connect(self):
+    return self._connected and self._socket_is_open()
